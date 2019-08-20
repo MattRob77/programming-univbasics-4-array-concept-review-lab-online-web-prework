592ac44722495e5f5e83639aeec0efc97c83973b
Matthew Robinson <matt_robinson1@yahoo.com>
2019-08-20 21:37:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -17,7 +17,7 @@ def find_element_index(array, value_to_find)
 def find_min_value(array)
   x = array[0]
    array.length.times do |index|
-   if array[index] > x 
+   if array[index] < x 
      x = array[index]
  end 
  end 
